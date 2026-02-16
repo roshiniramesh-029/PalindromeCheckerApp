15bff372f6cdf2672c8abfd452fff1b6c2b70131
roshiniramesh-029 <roshiniramesh2525@gmail.com>
2026-02-16 11:05:22 +0530

    uc1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,14 @@
+/*
+*@author dev
+* @version 1.0
+*
+*/
+
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("welcome to the Palindrome manager system");
+        System.out.println("Version 1.0");
+        System.out.println("System initialized successfully");
+    }
+
+}
